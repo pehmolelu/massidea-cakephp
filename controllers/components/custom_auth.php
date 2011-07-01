6ba78b7f8144853f4c1fb9a334d1f9bf11c0d057
jaakkop <jaakko.paukamainen@student.samk.fi>
2011-07-01 10:46:45 +0300

    Comment lines cleanup

--- controllers/components/custom_auth.php
+++ controllers/components/custom_auth.php
@@ -50,13 +50,6 @@ class CustomAuthComponent extends AuthComponent {
 			$salt = $this->User->getUserSalt($data['User.username']);
 			$data['User.password'] = $this->hashOldPassword($salt, $data['User.password']);
 			if($user = $this->identify($data)) {
-				/*
-				$userId = $this->User->find('list', array(
-					'conditions' => array('User.username' => $data['User.username']),
-					'fields' => array('User.id')
-				));
-				$userId = current($userId);
-				*/
 				$data['User.password'] = $this->updateToNewSalting($userId, $plainPassword);
 			}
 		} else {
@@ -67,7 +60,6 @@ class CustomAuthComponent extends AuthComponent {
 			// Continue authentication
 			if($this->Token->hasPendingActivation($userId)) {
 				$this->Session->setFlash(__('Login failed due to pending email verification.', true));
-				//$this->redirect('/users/login');
 				$success = false;
 			} else {
 				if($success = parent::login($data)) {
@@ -120,47 +112,5 @@ class CustomAuthComponent extends AuthComponent {
 	function hashOldPassword($salt = null, $password = null) {
 		return md5($salt.$password.$salt);
 	}
-	
-	
+		
 }
-/*
-		if(!empty($this->data)) {
-			// Get user record by username
-			$dbUserData = $this->User->findByUsername($this->data['User']['username']);
-			
-			if(!empty($dbUserData)) {
-				$isOldUser = $dbUserData['User']['password_salt'] != '';
-				$plainPassword = $this->Auth->data['User']['password'];
-				// Find out what kind of hashing is used
-				if(!$isOldUser) {
-					// Hash password
-					$this->data['User']['password'] = $plainPassword;
-					$this->data = $this->User->customHashPasswords($this->data);
-				} else {
-					// Fallback, try to identify with older hashing algorithm
-					$this->data = $this->User->hashOldPassword($dbUserData, $plainPassword);
-					// If user identifies with old hashing algorithm
-					if($this->Auth->identify($this->data)) {
-						// Update user record for new hash (hashing occurs in UserModel, beforeSave())
-						$this->data['User']['password'] = $plainPassword;
-						$this->data['User']['password_salt'] = '';
-						$this->data = $this->User->save($this->data, array(
-							'validate' => false, 
-							'fieldList' => array('password', 'password_salt')
-						));
-						if(!empty($this->data)) CakeLog::write('activity', 'Updated password hashing for user '.$this->data['User']['username']);
-					}
-				}
-				// Continue authentication
-				if(!$this->Token->hasPendingActivation($dbUserData['User']['id'])) {
-					if($this->Auth->login($this->data)) {
-						$this->Session->setFlash(__('Successfully logged in!', true));
-						$this->redirect('/');
-					}
-				} else {
-					$this->Session->setFlash(__('Login failed due to pending email verification.', true));
-					$this->redirect('/users/login');
-				}
-			}
-		}
- */
